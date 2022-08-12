f95d47c242bb00cc0a6785d539868cf82286dd74
Alexander Kensert <alexander.kensert@gmail.com>
2022-08-12 16:25:10 +0200

    remove dependency

--- molgraph/chemistry/benchmark/splitters.py
+++ molgraph/chemistry/benchmark/splitters.py
@@ -1,6 +1,5 @@
 from rdkit import Chem
 from rdkit.Chem.Scaffolds import MurckoScaffold
-from sklearn.model_selection import train_test_split
 from typing import List
 from typing import Optional
 from typing import Union
