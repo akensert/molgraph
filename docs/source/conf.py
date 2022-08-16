2be89f6bb238868a7c28137c4897788c7b5dfa49
Alexander Kensert <alexander.kensert@gmail.com>
2022-08-16 22:10:42 +0200

    add nbsphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.autosummary',
     'sphinx_gallery.load_style',
+    'nbsphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
